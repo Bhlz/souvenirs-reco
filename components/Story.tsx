e00d0b0ca5ab02691b824d73876b3848baef9eeb
Braulio Hernandez <brauliohernandez@Braulios-MacBook-Pro.local>
2025-08-31 19:30:52 -0600

    tu mensaje de cambio

--- components/Story.tsx
+++ components/Story.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 
-const BG_SRC = '/uploads/fondo-story.png'; // ← pon tu imagen aquí (public/uploads/story-bg.jpg)
+const BG_SRC = '/uploads/fondo-page.png'; // ← pon tu imagen aquí (public/uploads/story-bg.jpg)
 
 export default function Story() {
   const [visibleElements, setVisibleElements] = useState<Set<string>>(new Set());
